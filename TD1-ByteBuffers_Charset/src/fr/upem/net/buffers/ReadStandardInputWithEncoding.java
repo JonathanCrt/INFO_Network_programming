12de052be9fa4748521c9267f2e276dc7e72628d
Jonathan <jonathan.crete@outlook.fr>
2020-03-04 21:19:26 +0100

    [ADD &UPD] Add end of exo 2 + exo 3 of TD1

--- TD1-ByteBuffers_Charset/src/fr/upem/net/buffers/ReadStandardInputWithEncoding.java
+++ TD1-ByteBuffers_Charset/src/fr/upem/net/buffers/ReadStandardInputWithEncoding.java
@@ -14,27 +14,40 @@ public class ReadStandardInputWithEncoding {
 
     private static final int BUFFER_SIZE = 1024;
 
-    private static void usage(){
+    private static void usage() {
         System.out.println("Usage: ReadStandardInputWithEncoding charset");
     }
 
 
-
+    /**
+     * Read all the bytes from the standard input and return the corresponding string.
+     *
+     * @param cs
+     * @return
+     * @throws IOException
+     */
     private static String stringFromStandardInput(Charset cs) throws IOException {
         try (var channel = Channels.newChannel(System.in)) {
             ByteBuffer buff = ByteBuffer.allocate(BUFFER_SIZE);
+            while (channel.read(buff) != -1) { // Tant que la lecture du fileChannel ne renvoie pas -1.
+                if (!buff.hasRemaining()) { // Si tous les octets ont etes lus
+                    int newCapacity = buff.capacity() * 2; // On double la capacite
+                    ByteBuffer.allocate(newCapacity).put(buff.flip()); // Et on alloue un nouveau buffer avec
+                    // une nouvelle capacite et on ecrit les octets du buffers relus
+                }
 
+            }
+            return cs.decode(buff.flip()).toString();
         }
-        return null;
     }
 
     public static void main(String[] args) throws IOException {
-        if (args.length!=1){
+        if (args.length != 1) {
             usage();
             return;
         }
-        Charset cs=Charset.forName(args[0]);
-        System.out.print(stringFromStandardInput(cs));
+        Charset cs = Charset.forName(args[0]);
+        System.out.print(this.stringFromStandardInput(cs));
 
 
     }
